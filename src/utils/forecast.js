237ff61694c60b0c0f029bf7fd1d7bb68e7e3d97
Aryan Todi <aryantodi13@gmail.com>
2021-06-26 19:36:20 +0530

    Display max and min temperature of the day

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (lat, long, callback) => {
-    const url = 'http://api.weatherapi.com/v1/current.json?key=7d352bb82d9b4c82a16130936211906&q=' + encodeURIComponent(lat) + ',' + encodeURIComponent(long);
+    const url = 'http://api.weatherapi.com/v1/forecast.json?key=7d352bb82d9b4c82a16130936211906&q=' + encodeURIComponent(lat) + ',' + encodeURIComponent(long);
     request({url, json: true}, (error, {body}) => {
         if(error){
             callback('Could not connect!', undefined);
@@ -10,7 +10,8 @@ const forecast = (lat, long, callback) => {
             callback(body.error.message, undefined);
         }
         else{
-            const str = body.location.name + ': ' + body.current.condition.text + '. Temperature is ' + body.current.temp_c + ' C , and precipitation is ' + body.current.precip_in + ' in.';
+            var str = body.current.condition.text + '. Temperature is ' + body.current.temp_c + ' C , and precipitation is ' + body.current.precip_in + ' in.';
+            str = str + ' Maximum temperature today was ' + body.forecast.forecastday[0].day.maxtemp_c + ' C , and minimum temperature today was ' + body.forecast.forecastday[0].day.mintemp_c + ' C.'
             callback(undefined, str);
         }
     });
