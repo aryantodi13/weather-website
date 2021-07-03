c5a53c167a4d03df36c7f388ee5584ff9625bb86
Aryan Todi <aryantodi13@gmail.com>
2021-07-03 19:10:23 +0530

    Added option to get forecast of current location

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 const weatherForm = document.querySelector('form');
+const myLocation = document.querySelector('#myLocation')
 const address = document.querySelector('input');
 const message1 = document.querySelector('#message-1');
 const message2 = document.querySelector('#message-2');
@@ -18,4 +19,26 @@ weatherForm.addEventListener('submit', (e) => {
             }
         })
     })
+})
+myLocation.addEventListener('click', (e) => {
+    if(!navigator.geolocation) return alert('Your browser does not support geolocation') 
+    message1.textContent = 'Loading...';
+    message1.textContent = '';
+    var coords;
+    navigator.geolocation.getCurrentPosition((position) => {
+        coords = (position.coords.longitude + ',' + position.coords.latitude)
+        var url = '/weather?address=' + coords;
+        fetch(url).then((response) => {
+            response.json().then((data) => {
+                if(data.error){
+                    message1.textContent = data.error;
+                }
+                else{
+                    message1.textContent = data.location;
+                    message2.textContent = data.forecast;
+                }
+            })
+        })
+    })
+
 })
